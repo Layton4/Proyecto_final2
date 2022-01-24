bf1040f482c294b3864283c89a0cf8af25525d39
Layton4 <Javierete4774@gmail.com>
2022-01-24 11:16:12 +0100

    probando_boton

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private float  HorizontalInput;
+    private float VerticalInput;
+    public float speed = 5f;
+    public float rotatesepeed = 5f;
+    private Animator boton;
+    private Animator door;
+    void Start()
+    {
+        boton = GameObject.Find("button").GetComponent<Animator>();
+        door = GameObject.Find("puerta").GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        HorizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(speed * Vector3.forward * VerticalInput * Time.deltaTime);
+        transform.Rotate(rotatesepeed * Vector3.up * HorizontalInput * Time.deltaTime);
+
+    }
+
+    private void OnTriggerEnter(Collider othercollider)
+    {
+        if (othercollider.gameObject.CompareTag("button"))
+        {
+            boton.SetBool("press", true);
+            door.SetBool("open", true);
+        }
+    }
+    
+
+}
